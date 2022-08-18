101e614744d4e51762745e0a8546f0784491bc2c
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2022-08-18 03:51:52 -0700

    Add mouse dragging + scrolling functionality

--- Common/src/main/java/com/example/examplemod/client/WorldScreen.java
+++ Common/src/main/java/com/example/examplemod/client/WorldScreen.java
@@ -1,12 +1,15 @@
 package com.example.examplemod.client;
 
 import com.example.examplemod.mixin.UtilAccess;
+import com.example.examplemod.mixin.client.KeyMappingAccess;
+import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.blaze3d.vertex.PoseStack;
 import it.unimi.dsi.fastutil.longs.LongArrayList;
 import it.unimi.dsi.fastutil.longs.LongList;
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 import it.unimi.dsi.fastutil.longs.LongSet;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.server.IntegratedServer;
@@ -21,10 +24,13 @@ import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.FastColor;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import org.jetbrains.annotations.NotNull;
+import org.lwjgl.glfw.GLFW;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,16 +40,16 @@ import java.util.concurrent.ExecutorService;
 public class WorldScreen extends Screen {
 
 
-    private static final ExecutorService EXECUTOR_SERVICE = UtilAccess.invokeMakeExecutor("world-viewer");
+    private ExecutorService executorService = UtilAccess.invokeMakeExecutor("world-viewer");
 
 
     List<CompletableFuture<List<Tile>>> tiles = new ArrayList<>();
 
     private final ServerLevel level;
 
-    private BlockPos origin;
+    private BlockPos.MutableBlockPos origin;
 
-    private int resolution;
+    private int scrollCooldown;
 
     private LongSet submitted = new LongOpenHashSet();
 
@@ -52,6 +58,8 @@ public class WorldScreen extends Screen {
     int size = 16;
 
     private final List<Tile> toRender = new ArrayList<>();
+
+    private BoundingBox worldArea;
 
 
     public WorldScreen(Component $$0) {
@@ -63,60 +71,81 @@ public class WorldScreen extends Screen {
             colorForBiome.put(possibleBiome, FastColor.ARGB32.color(255, level.random.nextInt(256), level.random.nextInt(256), level.random.nextInt(256)));
         }
 
-        origin = Minecraft.getInstance().player.blockPosition();
+        BlockPos playerBlockPos = Minecraft.getInstance().player.blockPosition();
+        origin = new BlockPos.MutableBlockPos().set(playerBlockPos).setY(Mth.clamp(playerBlockPos.getY(), this.level.getMinBuildHeight(), this.level.getMaxBuildHeight()));
+        setWorldArea();
+    }
 
+    private void setWorldArea() {
+        int screenCenterX = (int) ((this.width / 2) / scale);
+        int screenCenterZ = (int) ((this.height / 2) / scale);
 
+        int xRange = SectionPos.blockToSectionCoord(screenCenterX) + 1;
+        int zRange = SectionPos.blockToSectionCoord(screenCenterZ) + 1;
+        this.worldArea = new BoundingBox(
+                this.origin.getX() - screenCenterX - SectionPos.sectionToBlockCoord(xRange),
+                level.getMinBuildHeight(),
+                this.origin.getZ() - screenCenterZ - SectionPos.sectionToBlockCoord(zRange),
+                this.origin.getX() + screenCenterX + SectionPos.sectionToBlockCoord(xRange),
+                level.getMaxBuildHeight(),
+                this.origin.getZ() + screenCenterZ + SectionPos.sectionToBlockCoord(zRange)
+        );
     }
 
     @Override
     public void tick() {
-        long originChunk = ChunkPos.asLong(this.origin);
-        int centerX = (int) ((this.width / 2) / scale);
-        int centerZ = (int) ((this.height / 2) / scale);
+        this.scrollCooldown--;
 
-        int xRange = SectionPos.blockToSectionCoord(centerX) + 1;
-        int zRange = SectionPos.blockToSectionCoord(centerZ) + 1;
+        if (scrollCooldown < 0) {
 
-        LongList tilesToSubmit = new LongArrayList();
-        for (int x = -xRange; x <= xRange; x++) {
-            for (int z = -zRange; z <= zRange; z++) {
-                int worldChunkX = ChunkPos.getX(originChunk) + x;
-                int worldChunkZ = ChunkPos.getZ(originChunk) + z;
-                long worldChunk = ChunkPos.asLong(worldChunkX, worldChunkZ);
-                if (submitted.add(worldChunk)) {
-                    tilesToSubmit.add(worldChunk);
+            long originChunk = ChunkPos.asLong(this.origin);
+            int centerX = (int) ((this.width / 2) / scale);
+            int centerZ = (int) ((this.height / 2) / scale);
+
+            int xRange = SectionPos.blockToSectionCoord(centerX) + 1;
+            int zRange = SectionPos.blockToSectionCoord(centerZ) + 1;
+
+            LongList tilesToSubmit = new LongArrayList();
+            for (int x = -xRange; x <= xRange; x++) {
+                for (int z = -zRange; z <= zRange; z++) {
+                    int worldChunkX = ChunkPos.getX(originChunk) + x;
+                    int worldChunkZ = ChunkPos.getZ(originChunk) + z;
+                    long worldChunk = ChunkPos.asLong(worldChunkX, worldChunkZ);
+                    if (submitted.add(worldChunk)) {
+                        tilesToSubmit.add(worldChunk);
+                    }
                 }
             }
-        }
 
-        int tilesPerThreadCount = (int) (100 / scale);
-        for (int i = 0; i < tilesToSubmit.size(); i += tilesPerThreadCount) {
-            LongList longList = tilesToSubmit.subList(i, Math.min(tilesToSubmit.size(), i + tilesPerThreadCount));
-            LongList tilesForFuture = new LongArrayList();
-            tilesForFuture.addAll(longList);
-            this.tiles.add(CompletableFuture.supplyAsync(
-                    () -> {
-                        ArrayList<Tile> tiles = new ArrayList<>();
-                        tilesForFuture.forEach(key -> {
-                            int worldX = SectionPos.sectionToBlockCoord(ChunkPos.getX(key));
-                            int worldZ = SectionPos.sectionToBlockCoord(ChunkPos.getZ(key));
-                            tiles.add(new Tile(63, worldX, worldZ, size, this.level::getBiome, this.colorForBiome));
-                        });
-                        return tiles;
-                    }, EXECUTOR_SERVICE)
-            );
-        }
-
-
-        List<CompletableFuture<List<Tile>>> completableFutures = this.tiles;
-        for (int i = 0; i < completableFutures.size(); i++) {
-            CompletableFuture<List<Tile>> tileList = completableFutures.get(i);
-            List<Tile> tileListNow = tileList.getNow(null);
-            if (tileListNow != null) {
-                this.toRender.addAll(tileListNow);
-                this.tiles.remove(i);
+            int tilesPerThreadCount = (int) (100 / scale);
+            for (int i = 0; i < tilesToSubmit.size(); i += tilesPerThreadCount) {
+                LongList longList = tilesToSubmit.subList(i, Math.min(tilesToSubmit.size(), i + tilesPerThreadCount));
+                LongList tilesForFuture = new LongArrayList();
+                tilesForFuture.addAll(longList);
+                this.tiles.add(CompletableFuture.supplyAsync(
+                        () -> {
+                            ArrayList<Tile> tiles = new ArrayList<>();
+                            tilesForFuture.forEach(key -> {
+                                int worldX = SectionPos.sectionToBlockCoord(ChunkPos.getX(key));
+                                int worldZ = SectionPos.sectionToBlockCoord(ChunkPos.getZ(key));
+                                tiles.add(new Tile(this.origin.getY(), worldX, worldZ, size, this.level::getBiome, this.colorForBiome));
+                            });
+                            return tiles;
+                        }, executorService)
+                );
             }
 
+
+            List<CompletableFuture<List<Tile>>> completableFutures = this.tiles;
+            for (int i = 0; i < completableFutures.size(); i++) {
+                CompletableFuture<List<Tile>> tileList = completableFutures.get(i);
+                List<Tile> tileListNow = tileList.getNow(null);
+                if (tileListNow != null) {
+                    this.toRender.addAll(tileListNow);
+                    this.tiles.remove(i);
+                }
+
+            }
         }
         super.tick();
     }
@@ -137,7 +166,7 @@ public class WorldScreen extends Screen {
 
         int worldX = this.origin.getX() - (screenCenterX - scaledMouseX);
         int worldZ = this.origin.getZ() - (screenCenterZ - scaledMouseZ);
-        MutableComponent toolTipFrom = new TextComponent(String.format("%s, %s, %s", worldX, 63, worldZ));
+        MutableComponent toolTipFrom = new TextComponent(String.format("%s, %s, %s", worldX, this.origin.getY(), worldZ));
 
 
         for (Tile tileToRender : this.toRender) {
@@ -161,10 +190,16 @@ public class WorldScreen extends Screen {
 
     @Override
     public void onClose() {
-        for (CompletableFuture<List<Tile>> tile : this.tiles) {
-            tile.cancel(true);
-        }
+        terminateAllFutures();
         super.onClose();
+    }
+
+    private void terminateAllFutures() {
+        executorService.shutdown();
+
+        while (!executorService.isShutdown()) {
+        }
+        executorService = UtilAccess.invokeMakeExecutor("world-viewer");
     }
 
     @NotNull
@@ -179,5 +214,51 @@ public class WorldScreen extends Screen {
             hoverText = new TextComponent(location.toString());
         }
         return hoverText;
+    }
+
+    @Override
+    public boolean mouseDragged(double mouseX, double mouseY, int button, double dragX, double dragY) {
+        this.origin.move((int) (dragX / scale), 0, (int) (dragY / scale));
+        setWorldArea();
+
+        List<Tile> render = this.toRender;
+        for (int i = 0; i < render.size(); i++) {
+            Tile tileToRender = render.get(i);
+            int worldX = tileToRender.getWorldX();
+            int worldZ = tileToRender.getWorldZ();
+            if (!this.worldArea.intersects(worldX, worldZ, worldX, worldZ)) {
+                this.toRender.remove(i);
+                this.submitted.remove(ChunkPos.asLong(SectionPos.blockToSectionCoord(worldX), SectionPos.blockToSectionCoord(worldZ)));
+            }
+        }
+
+        return super.mouseDragged(mouseX, mouseY, button, dragX, dragY);
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double delta) {
+        if (isKeyOrMouseButtonDown(this.minecraft, this.minecraft.options.keyShift)) {
+            if (!this.level.isOutsideBuildHeight((int) (this.origin.getY() + delta))) {
+                this.origin.move(0, (int) delta, 0);
+                this.submitted.clear();
+                terminateAllFutures();
+                this.toRender.clear();
+            }
+        } else {
+            this.scale = (float) Mth.clamp(this.scale + (delta * 0.05), 0.2, 1.5);
+        }
+        this.scrollCooldown = 30;
+        return true;
+    }
+
+    public static boolean isKeyOrMouseButtonDown(Minecraft minecraft, KeyMapping keyMapping) {
+        InputConstants.Key key = ((KeyMappingAccess) keyMapping).wv_getKey();
+        long window = minecraft.getWindow().getWindow();
+        int keyValue = key.getValue();
+        if (key.getType() == InputConstants.Type.MOUSE) {
+            return GLFW.glfwGetMouseButton(window, keyValue) == 1;
+        } else {
+            return InputConstants.isKeyDown(window, keyValue);
+        }
     }
 }
---
+++ Common/src/main/java/com/example/examplemod/mixin/client/KeyMappingAccess.java
@@ -0,0 +1,13 @@
+package com.example.examplemod.mixin.client;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(KeyMapping.class)
+public interface KeyMappingAccess {
+
+    @Accessor("key")
+    InputConstants.Key wv_getKey();
+}
