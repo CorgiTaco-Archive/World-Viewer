f26693294c67354dbbc76ce294ea1ec06d53ac3a
⸸ ᴄʜᴀᴏᴛᴛɪᴄ ⸸ <64570529+chaottic@users.noreply.github.com>
2023-07-29 19:33:48 +1000

    draw quad

--- common/src/main/java/dev/corgitaco/worldviewer/client/Mesh.java
+++ common/src/main/java/dev/corgitaco/worldviewer/client/Mesh.java
@@ -4,35 +4,90 @@ import org.joml.Matrix4f;
 import org.lwjgl.system.MemoryStack;
 
 import static org.lwjgl.opengl.GL45.*;
+import static org.lwjgl.system.MemoryUtil.memCalloc;
+import static org.lwjgl.system.MemoryUtil.memFree;
 
 public final class Mesh implements Destroyable {
     private final int vao = glCreateVertexArrays();
     private final int vbo;
     private final int ebo;
     private final int ubo;
+    private final int ssbo;
 
     {
         try (var stack = MemoryStack.stackPush()) {
-            var buffers = stack.callocInt(3);
+            var buffers = stack.callocInt(4);
 
             glCreateBuffers(buffers);
 
             vbo = buffers.get(0);
             ebo = buffers.get(1);
-            ubo = buffers.get(3);
+            ubo = buffers.get(2);
+            ssbo = buffers.get(3);
+
+            setup();
         }
     }
 
+    private void setup() {
+        glVertexArrayVertexBuffer(vao, 0, vbo, 0, 16);
+
+        var buffer = memCalloc((4 * 4 + 6) * 4);
+
+        buffer.putFloat(-0.5F).putFloat(-0.5F).putFloat(0.0F).putFloat(1.0F);
+        buffer.putFloat( 0.5F).putFloat(-0.5F).putFloat(0.0F).putFloat(1.0F);
+        buffer.putFloat( 0.5F).putFloat( 0.5F).putFloat(0.0F).putFloat(1.0F);
+        buffer.putFloat(-0.5F).putFloat( 0.5F).putFloat(0.0F).putFloat(1.0F);
+
+        buffer.putInt(0).putInt(1).putInt(2).putInt(2).putInt(3).putInt(0);
+
+        buffer.flip();
+
+        var vertices = 4 * 4 * 4;
+        var elements = 6 * 4;
+
+        var flags = 0;
+
+        buffer.limit(vertices);
+        glNamedBufferStorage(vbo, buffer, flags);
+        buffer.position(vertices);
+
+        buffer.limit(vertices + elements);
+        glNamedBufferStorage(ebo, buffer, flags);
+        buffer.position(0);
+
+        memFree(buffer);
+
+        glVertexArrayElementBuffer(vao, ebo);
+    }
+
     public void draw(ProgramPipeline pipeline) {
-        pipeline.bind();
+        // pipeline.bind();
 
-        glDrawElementsInstanced(GL_TRIANGLES, 0, GL_UNSIGNED_INT, 0, 1);
+        glBindVertexArray(vao);
 
-        ProgramPipeline.unbind();
+        glEnableVertexArrayAttrib(vao, 0);
+
+        glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0, 1);
+
+        glDisableVertexArrayAttrib(vao, 0);
+
+        glBindVertexArray(0);
+
+        // ProgramPipeline.unbind();
     }
 
     // Multiply matrices before uploading to save overhead.
     public void uploadMatrices(Matrix4f matrix4f) {
+        var mapped = glMapNamedBuffer(ubo, GL_READ_WRITE);
+        if (mapped != null) {
+            matrix4f.get(mapped);
+        }
+
+        glUnmapNamedBuffer(ubo);
+    }
+
+    public void uploadInstance() {
 
     }
 
@@ -42,5 +97,6 @@ public final class Mesh implements Destroyable {
         glDeleteBuffers(vbo);
         glDeleteBuffers(ebo);
         glDeleteBuffers(ubo);
+        glDeleteBuffers(ssbo);
     }
 }
--- common/src/test/java/dev/corgitaco/worldviewer/WindowTest.java
+++ common/src/test/java/dev/corgitaco/worldviewer/WindowTest.java
@@ -1,11 +1,13 @@
 package dev.corgitaco.worldviewer;
 
+import dev.corgitaco.worldviewer.client.Mesh;
 import org.joml.Matrix4f;
 import org.junit.jupiter.api.Test;
 import org.lwjgl.opengl.GL;
 
 import static org.lwjgl.glfw.GLFW.glfwInit;
 import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.opengl.GL11.*;
 
 public final class WindowTest {
 
@@ -24,10 +26,17 @@ public final class WindowTest {
         var projection = new Matrix4f();
         var model = new Matrix4f();
 
+        var mesh = new Mesh();
+
+        glClearColor(1.0F, 0.0F, 0.0F, 0.0F);
+
         while (!window.shouldClose()) {
+            glClear(GL_COLOR_BUFFER_BIT);
 
             projection.identity();
             model.identity();
+
+            mesh.draw(null);
 
             window.update();
         }
